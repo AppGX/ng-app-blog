196f1adf7b5b2eb5c29af1d3581a16458ee28288
apocalupses <dmirtykrivobokov@gmail.com>
2019-12-06 00:38:48 +0600

    master

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from 'src/app/shared/interfaces';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthService {
+    
 
 constructor(private http: HttpClient) { }
-login(user: User){
 
+get token(): string{
+    return ''
+}
+private setToken(){
+
+}
+
+login(user: User):Observable<any>{
+    return this.http.post('', user)
+}
+
+logout(){
+
+}
+
+isAuthenticated():boolean{
+    return !!this.token;
 }
 }
 
