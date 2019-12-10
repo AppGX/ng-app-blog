ccb02dea1151a43ecae021f09785d4b1b0b422c6
AppGX <apocalypsis@mail.ru>
2019-12-10 16:55:51 +0600

    Отображение поста

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,3 +1,8 @@
+import { switchMap } from 'rxjs/operators';
+import { Post } from 'src/app/shared/interfaces';
+import { Observable } from 'rxjs';
+import { PostService } from 'src/app/shared/post.service';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  post$: Observable<Post>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private post: PostService) { }
 
   ngOnInit() {
+    this.post$ = this.route.params
+    .pipe(switchMap((params: Params) => {
+      return this.post.getById(params['id']);
+    }));
   }
 
 }
